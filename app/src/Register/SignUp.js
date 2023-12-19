ce8c5ff184da4ff07cba0c6eeb374deab32f9026
rilindselmani <rilindselmani01@gmail.com>
2023-12-19 02:45:05 +0100

    #

--- app/src/Register/SignUp.js
+++ app/src/Register/SignUp.js
@@ -1,10 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-=======
-import {  useNavigate } from "react-router-dom";
->>>>>>> 54673d99bcff9bea2f2775c9ba74cadce24dd99e
 import "./SignUp.css";
-import { Link } from "react-router-dom";
 import img2 from "../Images/img2.jpg";
 
 const SignUp = () => {
@@ -24,32 +19,6 @@ const SignUp = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="user signupBx">
-      <div className="formBx">
-        <form onSubmit={handleSubmit}>
-          <h2>Create an account</h2>
-          <input type="text" name="username" placeholder="Username" />
-          <input type="email" name="email" placeholder="Email Address" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Create Password"
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-          />
-          <input type="submit" value="Sign Up" />
-          <p className="signup">
-            Already have an account? <Link to="login">Sign In.</Link>
-          </p>
-        </form>
-      </div>
-      <div className="imgBx">
-        <img src={img2} alt="img2" />
-=======
     <div className="container">
       <div className="user signupBx">
         <div className="formBx">
@@ -87,7 +56,6 @@ const SignUp = () => {
         <div className="imgBx">
           <img src={img2} alt="img2" />
         </div>
->>>>>>> 54673d99bcff9bea2f2775c9ba74cadce24dd99e
       </div>
     </div>
   );
