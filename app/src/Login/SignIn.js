ebe08d8ba7f890038a7b2716b1915a632b4801d3
rilindselmani <rilindselmani01@gmail.com>
2023-12-19 04:05:28 +0100

    #

--- app/src/Login/SignIn.js
+++ app/src/Login/SignIn.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./SignIn.css";
 import img1 from "../Images/img1.jpg";
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Add your login logic here
 
-    // Redirect to the home page after successful login
+    if (password.length < 8 || password.length > 16 || !/\d/.test(password)) {
+      setPasswordError(true);
+      return;
+    } else {
+      setPasswordError(false);
+    }
+
     navigate("/");
   };
 
@@ -27,13 +34,27 @@ const SignIn = () => {
               <input type="text" name="username" placeholder="Username" />
             </div>
             <div className="formGroup">
-              <input type="password" name="password" placeholder="Password" />
+              <input
+                type="password"
+                name="current-password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ border: passwordError ? "1px solid red" : "" }}
+              />
+              {passwordError && (
+                <p style={{ color: "red", fontSize: "12px" }}>
+                  Password must be between 8 and 16 characters long and contain
+                  at least one digit.
+                </p>
+              )}
             </div>
             <div className="formGroup">
               <input type="submit" value="Login" />
             </div>
-            <p className="signup">Don't have an account?</p>
-            <Link to="/signup">Sign Up.</Link>
+            <p className="signup">
+              Don't have an account? <Link to="/register">Sign Up.</Link>
+            </p>
           </form>
         </div>
       </div>
--- app/src/Register/SignUp.js
+++ app/src/Register/SignUp.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
-import { useNavigate } from "react";
 import img2 from "../Images/img2.jpg";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    // Password validation: between 8 and 16 characters including at least one digit
+    if (password.length < 8 || password.length > 16 || !/\d/.test(password)) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+
+    // Additional validation, e.g., check if passwords match
+    if (password !== confirmPassword) {
+      setConfirmPasswordError(true);
+      return;
+    } else {
+      setConfirmPasswordError(false);
+    }
+
     // Perform your form submission logic here
 
     // Redirect to the login page after successful form submission
-    navigate("/login");
-  };
-
-  const handleLoginRedirect = () => {
-    // Redirect to the login page when the button is clicked
     navigate("/login");
   };
 
@@ -26,31 +44,63 @@ const SignUp = () => {
           <form onSubmit={handleSubmit}>
             <h2>Create an account</h2>
             <div className="formGroup">
-              <input type="text" name="username" placeholder="Username" />
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="formGroup">
-              <input type="email" name="email" placeholder="Email Address" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                autoComplete="username"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="formGroup">
               <input
                 type="password"
                 name="password"
                 placeholder="Create Password"
+                autoComplete="new-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ border: passwordError ? "1px solid red" : "" }}
               />
+              {passwordError && (
+                <p style={{ color: "red", fontSize: "12px" }}>
+                  Password must be between 8 and 16 characters long and contain
+                  at least one digit.
+                </p>
+              )}
             </div>
             <div className="formGroup">
               <input
                 type="password"
                 name="confirmPassword"
                 placeholder="Confirm Password"
+                autoComplete="new-password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                style={{ border: confirmPasswordError ? "1px solid red" : "" }}
               />
+              {confirmPasswordError && (
+                <p style={{ color: "red", fontSize: "12px" }}>
+                  Passwords do not match.
+                </p>
+              )}
             </div>
             <div className="formGroup">
               <input type="submit" value="Sign Up" />
             </div>
             <p className="signup">
               Already have an account?
-              <button onClick={handleLoginRedirect}>Sign In.</button>
+              <Link to="/login">Sign in.</Link>
             </p>
           </form>
         </div>
